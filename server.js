422c5cb93127e6218b57f22cdc25866723baae69
heatherzen <heather.zen@gmail.com>
2020-08-12 21:30:15 -0700

    mod 11.3.6, trouble finding where to put zookeepers route

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 const { animals } = require('./data/animals');
 
@@ -117,6 +118,20 @@ app.post('/api/animals', (req, res) => {
     //req.body is where our incoming content wil be
     res.json(animal);
 });
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
